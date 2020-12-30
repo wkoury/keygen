37f54bfeb9b3d380023e74c923f7a5579065a5d3
Will Koury <wkoury@hey.com>
2020-12-30 10:38:07 -0500

    The real initial commit

--- keygen/main.c
+++ keygen/main.c
@@ -6,9 +6,37 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+unsigned char generateRandomChar(void);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    const int DEFAULT_LENGTH = 64;
+    
+    unsigned char ret[DEFAULT_LENGTH];
+    
+    //initialize the random number generator
+    time_t t;
+    srand((unsigned) time(&t));
+    
+    for(int i = 0; i < DEFAULT_LENGTH; ++i){
+        unsigned char rando = generateRandomChar();
+        ret[i] = rando;
+    }
+    
+    printf("%s\n", ret);
+    
     return 0;
+}
+
+//generate a random character, recursively disallowing invalid characters
+unsigned char generateRandomChar(){
+    unsigned char ret = (unsigned char)(rand() % 126 + 1);
+    if(ret < 33 || ret > 126){ //return a different character if we get one that is not allowed
+        return generateRandomChar();
+    }
+    
+    return ret;
 }
